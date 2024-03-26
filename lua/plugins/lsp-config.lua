2170f2f23b76fb7d9cade545dbd49e5372ab0530
Isaac Franco <isaacfranco@gmail.com>
2024-03-26 08:12:25 -0300

    init + stuff

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,49 +1,53 @@
 return {
-    {
-        -- mason take care of lang server installs
-        "williamboman/mason.nvim",
-        config = function()
-            require("mason").setup()
-        end
-    },
-    {
-        -- mason-lspconfig connects mason and lspconfig. ensures installed lang servers
-        "williamboman/mason-lspconfig.nvim",
-        config = function()
-            require("mason-lspconfig").setup({
-                -- availability on https://github.com/williamboman/mason-lspconfig.nvim
-                ensure_installed = {
-                   "lua_ls",
-                   "tsserver"
-                }
-            })
-        end
-    },
-    {
-        -- nvim-lspconfig (https://github.com/neovim/nvim-lspconfig)
-        -- configures neovim to send messages and connect to lang servers
-        -- responsible for keybindings as well
-        -- Help: :h vim.lsp.buf
-        "neovim/nvim-lspconfig",
-        config = function()
-            local lspconfig = require("lspconfig")
+	{
+		-- mason take care of lang server installs
+		"williamboman/mason.nvim",
+		config = function()
+			require("mason").setup()
+		end,
+	},
+	{
+		-- mason-lspconfig connects mason and lspconfig. ensures installed lang servers
+		"williamboman/mason-lspconfig.nvim",
+		config = function()
+			require("mason-lspconfig").setup({
+				-- availability on https://github.com/williamboman/mason-lspconfig.nvim
+				-- only lsps. no formatters (for example) can be ensured intalled here
+				ensure_installed = {
+					"lua_ls",
+					"tsserver",
+                    "biome"
+				},
+			})
+		end,
+	},
+	{
+		-- nvim-lspconfig (https://github.com/neovim/nvim-lspconfig)
+		-- configures neovim to send messages and connect to lang servers
+		-- responsible for keybindings as well
+		-- Help: :h vim.lsp.buf
+		"neovim/nvim-lspconfig",
+		config = function()
+			local lspconfig = require("lspconfig")
 
-            lspconfig.lua_ls.setup({})
-            lspconfig.tsserver.setup({})
+			lspconfig.lua_ls.setup({})
+			lspconfig.tsserver.setup({})
+			lspconfig.biome.setup({})
+			--lspconfig.eslint.setup({})
 
-            local opts = {}
-            vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-            vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-            vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-            vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-            vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
-            vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, opts)
-            vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-            vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action, opts)
-            vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-            vim.keymap.set('n', '<space>FF', function()
-                vim.lsp.buf.format { async = true }
-            end, opts)
-        end
-    }
+			local opts = {}
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+			vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+			vim.keymap.set("n", "<space>FF", function()
+				vim.lsp.buf.format({ async = true })
+			end, opts)
+		end,
+	},
 }
--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,13 +1,22 @@
 return {
     -- none-ls (fork of null-ls) enables non-lsp tools to behave like lsps whn using nvim
-    "nvimtools/none-ls.lua.nvim",
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require("null-ls")
         -- see more buitins on https://github.com/nvimtools/none-ls.nvim/blob/main/doc/BUILTINS.md
+        -- some sources are on none-ls-extas and should be required intead of using through builtins
         null_ls.setup({
             sources = {
-                null_ls.builtins.formatting.stylua
-            }
+--                require("none-ls.diagnostics.eslint_d"),
+                null_ls.builtins.formatting.stylua,
+                null_ls.builtins.diagnostics.rubocop,
+                null_ls.builtins.formatting.rubocop,
+                null_ls.builtins.formatting.prettier,
+            },
         })
-    end
+    end,
 }
