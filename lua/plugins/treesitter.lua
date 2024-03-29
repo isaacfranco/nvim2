67835cdfd56827ede4031cdcdb882f252b8ec23b
Isaac Franco <isaacfranco@gmail.com>
2024-03-29 09:47:16 -0300

    html

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
     config = function()
         local config = require("nvim-treesitter.configs")
         config.setup({
-            ensure_installed = { "lua", "javascript", "http", "xml", "graphql", "json" },
+            ensure_installed = { "lua", "javascript", "http", "xml", "graphql", "json", "html" },
             auto_install = true,
             highlight = { enable = true },
             indent = { enable = true },
